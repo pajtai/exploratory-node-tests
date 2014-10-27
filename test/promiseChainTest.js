7acf94575b55962a8b4a3784e6aed1af3a0408fe
Peter Ajtai <peter.ajtai@thinksolid.com>
2014-10-27 13:01:43 -0700

    some other tests

--- test/promiseChainTest.js
+++ test/promiseChainTest.js
@@ -8,31 +8,43 @@ var chai = require('chai'),
 describe('vanilla promise chain', function() {
     it('should add up all numbers', function(done) {
         add(0, 1)
-            .then(function (sum) {
-                console.log(sum, new Date().getTime());
-                return sum;
-             })
-             .then(function (sum) {
-                 return add(sum, 4);
-             })
-             .then(function (sum) {
-                console.log(sum, new Date().getTime());
-                return sum;
-             })
-             .then(function (sum) {
-                return add(sum, 2);
-             })
-             .then(function (sum) {
-                console.log(sum, new Date().getTime());
-                return sum;
-             })
+            .then(logSum)
+            .then(add.bind(null, 4))
+            .then(logSum)
+            .then(add.bind(null, 2))
+            .then(logSum)
              .then(function(sum) {
                  sum.should.equal(7);
                  done();
              })
              .catch(done);
     });
+
+    it('should add up all numbers & multiply if sum is greater than 4 after the first two add', function(done) {
+        add(0, 1)
+            .then(logSum)
+            .then(add.bind(null, 4))
+            .then(logSum)
+            .then(function(sum) {
+                if (sum > 4) {
+                    return multiply(sum, 2);
+                } else {
+                    return add(sum, 2);
+                }
+            })
+            .then(logSum)
+            .then(function(sum) {
+                sum.should.equal(10);
+                done();
+            })
+            .catch(done);
+    });
 });
+
+function logSum(sum) {
+    console.log(sum, new Date().getTime());
+    return sum;
+}
 
 describe('generator function', function () {
     it('should add up all numbers', function(done) {
@@ -48,9 +60,43 @@ describe('generator function', function () {
 
             num.should.equal(7);
             done();
-        })().catch(done);
+        })()
+            .catch(done);
+    });
+
+    it('should add up all numbers & multiply if sum is greater than 4 after the first two add', function(done) {
+        Q.async(function* () {
+            var num = 0;
+
+            num = yield add(num, 1);
+            logSum(num);
+            num = yield add(num, 4);
+            logSum(num);
+            if (num > 4) {
+                num = yield multiply(num, 2);
+                logSum(num);
+            } else {
+                num = yield add(num, 2);
+                logSum(num);
+            }
+
+
+            num.should.equal(10);
+            done();
+        })()
+            .catch(done);
     });
 });
+
+function multiply(num, otherNum) {
+    var def = Q.defer();
+
+    _.delay(function () {
+        def.resolve(num * otherNum);
+    }, 500);
+
+    return def.promise;
+}
 
 function add(num, otherNum) {
     var def = Q.defer();
